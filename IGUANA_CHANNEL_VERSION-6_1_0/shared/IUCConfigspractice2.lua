15dd8e6f099a124fbcf4b36f86c75e1b681bfde6
practice2 <practice2>
2018-09-21 04:42:15 +0000

    Export: okay

--- IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigspractice2.lua
+++ IGUANA_CHANNEL_VERSION-6_1_0/shared/IUCConfigspractice2.lua
@@ -1,27 +1,28 @@
-local groupName = 'GROUPNAME'
+local groupName = 'practice2'
 local envVariable = 'serverType'..groupName
 local serverType = os.getenv(envVariable)
 
 local iucConf = {}
 
+
 iucConf.dev = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/practice2/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/practice2/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
 
 iucConf.prod = {
    -- Configurations for patient csv File
-   ['patientCsvPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientCsvPath'] = '/home/centos/iuc/prod/practice2/',
    ['patientCsvFile'] = 'PatientData.txt',
 	
    -- Configurations for patient DB file
-   ['patientDbPath'] = '/home/centos/iuc/prod/GROUPNAME/',
+   ['patientDbPath'] = '/home/centos/iuc/prod/practice2/',
    ['patientDbFile'] = 'PatientData.sqlite'
 }
 
